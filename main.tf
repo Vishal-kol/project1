93b7ee92e69b1d2d6e47321d3fe07b7298920e17
Vishal‑kol <taylorweb36@gmail.com>
2026-06-28 21:10:26 +0530

    check

--- main.tf
+++ main.tf
@@ -1,9 +1,23 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 7.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.9"
+    }
+  }
+}
+
 provider "google" {
-  project     = "project-79a0f092-e287-4dd8-b88"
-  region      = "us-central1-a"
+  project = "project-79a0f092-e287-4dd8-b88"
+  region  = "us-central1"
 }
 
 resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
-git
