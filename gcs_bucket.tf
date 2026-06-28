4997378075c2cf77f9cbc64520445860186169ee
Vishal‑kol <taylorweb36@gmail.com>
2026-06-28 21:25:45 +0530

    check

--- gcs_bucket.tf
+++ gcs_bucket.tf
@@ -1,8 +1,6 @@
 resource "google_storage_bucket" "default" {
-  name          = "terraform-state-${random_id.bucket_suffix.hex}"
-  location      = "EU"
-  force_destroy = true
-
+  name                        = "terraform-state-36930707"
+  location                    = "EU"
+  force_destroy               = true
   uniform_bucket_level_access = true
-
 }
--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "gcs" {
+    bucket = "terraform-state-36930707"
+    prefix = "terraform/state"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
